6414b51b83ca12004f5606a32cbad74833743614
Vbik-713 <vikethhegde@gmail.com>
2026-04-10 12:08:44 +0530

    new

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn DEV OP WITH AWS
         </a>
       </header>
     </div>
